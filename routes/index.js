43dcb713ef95424075a3f8eed91c7bb8fa95cd26
Clemilson Morais <clemilsonmorais@gmail.com>
2018-12-13 09:23:03 -0300

    Adição dos códigos iniciais

---
+++ routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var opcoes = ['*Mini*'];
+var pedidos = [];
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express' , opcoes, pedidos});
+});
+
+
+router.get('/admin', (req, res, next) => {
+  res.render('admin', {pedidos, opcoes});
+});
+
+router.post('/enviar', (req, res, next) => {
+  if(req.body.nome && req.body.nome.length > 0) {
+    pedidos = pedidos.filter(p => p.nome != req.body.nome);
+    let ops = []
+    if (typeof(req.body.opcoes) == 'string') {
+      ops = [req.body.opcoes];
+    } else {
+      ops = req.body.opcoes;
+    }
+    pedidos.push({nome: req.body.nome, opcoes: ops});
+  }
+  res.redirect('/');
+});
+
+router.post('/inserirCardapio', (req, res, next) => {
+  let cardapio = req.body.cardapio.trim();
+  opcoes = ['*Mini*'];
+  if(cardapio) {
+    opcoes = opcoes.concat(cardapio.split('\r\n'));
+  }
+  pedidos = [];
+  res.redirect('/admin');
+});
+
+router.get('/remover/:nome', (req, res, next) => {
+  pedidos = pedidos.filter(p => p.nome != req.params.nome);
+  res.redirect('/admin');
+});
+
+
+router.get('/gerarPedido', (req, res, next) => {
+  res.render('gerar', {pedidos});
+});
+
+
+module.exports = router;
